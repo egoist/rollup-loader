f8f7f04947fec8895c48a016f6f4664f76970c4d
John-David Dalton <john.david.dalton@gmail.com>
2017-01-26 16:22:30 -0800

    Minor cleanup of loader. (#7)

--- src/index.js
+++ src/index.js
@@ -2,24 +2,16 @@ import rollup from 'rollup'
 import memory from 'rollup-plugin-memory'
 import loaderUtils from 'loader-utils'
 
-export default function (source, inputSourceMap) {
+export default function (contents) {
   const cb = this.async()
-
-  const webpackRemainingChain = loaderUtils.getRemainingRequest(this).split('!')
-  const filename = webpackRemainingChain.pop()
-
+  const path = loaderUtils.getRemainingRequest(this).split('!').pop()
   const rollupConfig = {
-    entry: {
-      path: filename,
-      contents: source
-    },
-    external(id) {
-      return !/\.(?:js|es6)$/.test(id)
-    }
+    entry: { path, contents },
+    external: RegExp().test.bind(/^(.(?!\.(js|es)$))*$/)
   }
 
-  const parsed = this.options.rollup || loaderUtils.parseQuery(this.query)
-  const options = Array.isArray(parsed) ? { plugins: parsed } : parsed
+  const plugins = this.options.rollup || loaderUtils.parseQuery(this.query)
+  const options = Array.isArray(parsed) ? { plugins } : plugins
 
   Object.assign(rollupConfig, options)
   rollupConfig.plugins = [memory(), ...options.plugins || []]
